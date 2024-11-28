b8c6f176add1c2cf1ef2acd080ac75cdaac6f643
rail-23 <railgonzalesaldana603@gmail.com>
2024-11-28 10:55:00 -0400

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import authRoutes from './routes/auth.routes';
-import userRoutes from './routes/user.routes';
-import licenseRoutes from './routes/licensia.routes';
+import authRoutes from './routes/auth.routes.js';
+
+import userRoutes from './routes/user.routes.js';
+import licenseRoutes from './routes/licensia.routes.js';
 
 const app = express();
 
