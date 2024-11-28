feda5eb7b5356cabfd93ae121dc69636ae4b8608
rail-23 <railgonzalesaldana603@gmail.com>
2024-11-28 16:03:43 -0400

    Update auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import * as authCtrl from '../controllers/auth.controller';
+import * as authCtrl from '../controllers/auth.controller.js';
 
 
 const router = Router();
